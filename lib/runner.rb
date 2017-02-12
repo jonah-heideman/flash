26dcdd1638e55c8c2f67ede5ed1f641789d30f01
Jonah Heideman <joeys@Jonahs-MacBook-Air.local>
2017-02-12 15:44:00 -0700

    make whitespace consistent

--- lib/runner.rb
+++ lib/runner.rb
@@ -3,12 +3,13 @@ require_relative 'round'
 require_relative 'card'
 
 class Runner
+
   def initialize(config)
-    @data = config[:data]
+    @data      = config[:data]
     @predicate = config[:predicate] || ''
     @game_name = config[:game_name]
-    @deck = Deck.new(init_cards.shuffle)
-    @round = Round.new(@deck)
+    @deck      = Deck.new(init_cards.shuffle)
+    @round     = Round.new(@deck)
   end
 
   def init_cards
